c114d860b4d5c4bef912d3830cfca48ddc50dadb
HeavyMetalMF <condrashov20123@mail.ru>
2022-05-29 12:33:17 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app">
+    <PostForm/>
+    <PostList vbi/>
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+export default {
+  components: {
+    PostForm,
+    PostList
+  },
+  name: "App",
+  data(){
+    return {
+      posts: [
+        {id: 1, title: 'description1', body: 'Post number 1'},
+        {id: 2, title: 'description2', body: 'Post number 2'},
+        {id: 3, title: 'description3', body: 'Post number 3'},
+      ],
+      title: '',
+      body: '',
+    }
+  },
+  methods:{
+    createPost(event){
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body : this.body,
+      }
+      this.posts.push(newPost)
+      this.title = ''
+      this.body = ''
+    },
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app{
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{posts}}
+  </div>
+<!--  <div class="post" v-for="post in posts">-->
+<!--    <div><strong>Name: </strong>{{post.title}}</div>-->
+<!--    <div><strong>Description: </strong>{{post.body}}</div>-->
+<!--  </div>-->
+</template>
+
+<script>
+export default {
+  name: "PostList",
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post{
+  padding: 15px;
+  border: 3px solid teal;
+  margin-top: 10px;
+}
+</style>
